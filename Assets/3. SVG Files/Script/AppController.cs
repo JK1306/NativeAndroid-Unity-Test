17f8a906fcae9eb85f37417bb19e6326a50e2310
Jaikishore <jaikishore@dlearners.in>
2024-05-31 18:12:39 +0530

    changes done

--- Assets/3. SVG Files/Script/AppController.cs
+++ Assets/3. SVG Files/Script/AppController.cs
@@ -4,15 +4,50 @@ using UnityEngine;
 
 public class AppController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] AudioSource playAudioSource;
+    [SerializeField] AudioClip[] clips;
+    [SerializeField] GameObject waveObject;
+    [SerializeField] GameObject playButton;
+    [SerializeField] GameObject pauseButton;
+    [SerializeField] Animator windowAnimation;
+
+    int currentSLide;
+
     void Start()
+    {
+        currentSLide = 1;
+        playAudioSource.GetComponent<Animator>().SetBool("stopAnimation", true);
+    }
+
+    void Update()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void OnButtonClickPlayAudio(){
+        if(playButton.activeSelf){
+            playAudioSource.GetComponent<Animator>().SetBool("stopAnimation", false);
+            playAudioSource.clip = clips[currentSLide - 1];
+            playAudioSource.Play();
+            pauseButton.SetActive(true);
+            playButton.SetActive(false);
+        }else if(pauseButton.activeSelf){
+            playAudioSource.GetComponent<Animator>().SetBool("stopAnimation", true);
+            playAudioSource.Stop();
+            pauseButton.SetActive(false);
+            playButton.SetActive(true);
+        }
+    }
+
+    public void OnButtonClickChangesSlide(int slideId){
+        if(currentSLide == slideId) return;
+        if(slideId == 1){
+            Debug.Log($"Came to IF statement");
+            windowAnimation.Play("window_change_animation_back");
+        }else{
+            Debug.Log($"Came to ELSE statement");
+            windowAnimation.Play("window_change_animation");
+        }
+        currentSLide = slideId;
     }
 }
